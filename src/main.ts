dd8b28111362b42bcf3f6a14304e1d2b54ac85d5
João <Joao.Neto@targetsistemas.com.br>
2022-03-14 14:29:42 -0300

    main.ts retirar cors e await no listen

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
   var port = process.env.PORT || 3000;
-  app.listen(port, "0.0.0.0", function() {
+  await app.listen(port, "0.0.0.0", function() {
     console.log("Listening on Port 3000");
     });
 }
