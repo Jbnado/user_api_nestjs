c572b1f766123b0a8f152ce9fed707522567b145
João <Joao.Neto@targetsistemas.com.br>
2022-03-14 14:21:46 -0300

    npm to engine and function to app.listen

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 8080);
+  var port = process.env.PORT || 3000;
+  app.listen(port, "0.0.0.0", function() {
+    console.log("Listening on Port 3000");
+    });
 }
 bootstrap();
